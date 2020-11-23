6df122e05b93eb99e2689704d08fb6b4434efc84
Tom Steele <tsteele@madisoncollege.edu>
2020-11-23 09:58:25 -0600

    Jasmine spies... : - (
    Not very fruitful, may try separating Biz Logic into a service by itself for testing...
    
    Testing is a big topic, maybe shouldn't even be covering it here ? ? ?

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -3,15 +3,50 @@ import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
 
+// Import so that we can create a 'test spy'...
+import { SwapiService } from '../swapi.service';
+
+import { HttpClient } from '@angular/common/http';
+
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  
+  // Blah...
+  let httpClientSpy: { get: jasmine.Spy };
+  let swapiServiceSpy: SwapiService;
 
   beforeEach(async(() => {
+  
+    // Create the swapiService spy...
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    swapiServiceSpy = jasmine.createSpyObj('SwapiService', ['get', 'fetchPlanets']);
+    //swapiServiceSpy = new SwapiService(httpClientSpy as any);
+  
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot()
+        //, HttpClient
+        //, SwapiService
+      ],
+      // providers: [
+      //   SwapiService
+      //   , HttpClient
+      // ]
     }).compileComponents();
+
+    TestBed.overrideComponent(
+      HomePage
+      , {
+        set: {
+          providers: [
+            {provide: HttpClient, useValue: httpClientSpy}
+            , {provide: SwapiService, useValue: swapiServiceSpy}
+          ]
+        }
+      }
+    );
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
